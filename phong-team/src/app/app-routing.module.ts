6dfe2f8572328f743b32a2208738900a678026e8
Francisco Quirant Vicente <fqv2@gcloud.ua.es>
2023-11-18 15:15:04 +0100

    Add landing page

--- phong-team/src/app/app-routing.module.ts
+++ phong-team/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Ruta de redirección para rutas no definidas
 
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  {
+    title: 'Inicio',
+    path: 'inicio',
+    component: LandingComponent,
+  },
+  {
+    // title: '',
+    // path: '',
+    // component: ,
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
